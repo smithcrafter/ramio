5da00c1bc55397d4dd26ba7160bf6ad77ffe1ddf
Vladimir Kuznetsov <smithcoder@yandex.ru>
2021-03-13 21:04:54 +0300

    0.1.5h11 ramio_all_headers - Include/ramio_all_headers.h

---
+++ Include/ramio_all_headers.h
@@ -0,0 +1,30 @@
+
+#include "../Source/ramio.h"
+
+// Items
+#include "../Source/Items/MetaTypes.h"
+#include "../Source/Items/MetaDescription.h"
+#include "../Source/Items/ItemData.h"
+#include "../Source/Items/MetaItemData.h"
+#include "../Source/Items/Item.h"
+#include "../Source/Items/StructItem.h"
+#include "../Source/Items/ItemObserver.h"
+#include "../Source/Items/ItemWatcher.h"
+#include "../Source/Items/AbstractListSet.h"
+#include "../Source/Items/AbstractMetaSet.h"
+#include "../Source/Items/StructItemSet.h"
+#include "../Source/Items/ItemSet.h"
+#include "../Source/Items/MetaItemSet.h"
+#include "../Source/Items/MetaItemSerialization.h"
+#include "../Source/Items/FilterItemSet.h"
+#include "../Source/Items/FilterMetaItemSet.h"
+#include "../Source/Items/Components.h"
+#include "../Source/Items/MetaItems.h"
+#include "../Source/Items/MetaStandardItems.h"
+
+// Database
+#include "../Source/Database/Database.h"
+#include "../Source/Database/DatabaseOptions.h"
+#include "../Source/Database/MetaTable.h"
+#include "../Source/Database/SqlQuery.h"
+
